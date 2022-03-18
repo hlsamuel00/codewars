9c3775b8e3766cf9d252c39b8e6f8492a9865cd9
Harvey S <hlsamuel00@yahoo.com>
2022-03-17 23:59:57 -0400

    Update 03-17-2022.js

--- 03-17-2022.js
+++ 03-17-2022.js
@@ -10,6 +10,31 @@
     let joinArray = reverseArray.join("")
     return joinArray
 }
-//I located this solution on google/stack-overflow. This was identical to an idea i had, but i will be sure to utilize pseudocode to breakdown pieces.
+//*****I located this solution on google/stack-overflow. This was identical to an idea i had, but i will be sure to utilize pseudocode to breakdown pieces.
+
 
 //=============================================================================================================
+
+
+// Given an array of integers. Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative. If the input is an empty array or is null, return an empty array.
+// ex:
+    // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+    function countPositivesSumNegatives(input){
+        let arr = []
+        let count = 0
+        let sum = 0
+        if (input == null || input.length == 0){
+          return arr
+        } else {input.forEach( element => {
+            if (element > 0){
+              count++
+              arr = arr.map(x => count)
+          } else if (element < 0){
+              sum += element
+            } 
+            arr = [count, sum]
+          })}
+        return arr;
+    }
+//*****This one went beyond the 20 minute time that Leon recommended, but i got it to work for actual inputs, but had to google how to make it return an empty array for null or blank array inputs. I am excited at how far I've come and eager to see where I'll be in a month's time. 
