c7e9d4f43ffee0662aa7e4acf14f47c1739623e4
Harvey S <hlsamuel00@yahoo.com>
2022-03-24 13:07:59 -0400

    Create 03-24-2022.js

---
+++ 03-24-2022.js
@@ -0,0 +1,10 @@
+// Implement a function which multiplies two numbers.
+
+let multiply = (a,b) => a * b;
+
+//*****This was simple, but it took me a minute to figure out because it wasn't noticing the Kata was requesting that the function be named 'multiply'.
+
+
+//=============================================================================================================
+
+
