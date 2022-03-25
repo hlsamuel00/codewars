79312857c6519c7389c18a5f11a04268df5ea063
Harvey S <hlsamuel00@yahoo.com>
2022-03-25 09:37:30 -0400

    Update 03-25-2022.js

--- 03-25-2022.js
+++ 03-25-2022.js
@@ -29,3 +29,18 @@ function mergeArrays(arr1, arr2) {
 
 
 //=============================================================================================================
+
+
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+//      Ex:
+//      ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+function smash (words) {
+    return words.join(' ')
+};
+
+//*****This was really easy to complete. I was able to do this with just the memory of what we've been working on in class. 
+
+
+//=============================================================================================================
