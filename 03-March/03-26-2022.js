b2f5a3c05cb41a9185b2135bcac6e10f1b634568
Harvey S <hlsamuel00@yahoo.com>
2022-03-26 17:03:21 -0400

    Update 03-26-2022.js

--- 03-March/03-26-2022.js
+++ 03-March/03-26-2022.js
@@ -35,3 +35,36 @@ function firstNonConsecutive (arr) {
 
 
 //=============================================================================================================
+
+
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//     ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+//   For example, if this array were passed as an argument:
+
+//     ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+//  Your function would return the following array:
+
+//     ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    birds = birds.filter(element => {
+        geese.forEach(x => element != x)
+    } )
+    return birds
+    // return an array containing all of the strings in the input array except those that match strings in geese
+  };
+
+  //*****I wasn't able to get this Kata either. This one was a bit tricky, but when i reviewed the solutions, they made sense. The solution below made the most sense and i will commit to understanding this in the future:
+
+//   function gooseFilter (birds) {
+//     var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+//     return birds.filter(b => !geese.includes(b));
+//   };
