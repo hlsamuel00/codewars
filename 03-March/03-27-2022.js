d282711102cec4bbc8f505d23665422ee1d65ccf
Harvey S <hlsamuel00@yahoo.com>
2022-03-27 15:57:31 -0400

    Update 03-27-2022.js

--- 03-March/03-27-2022.js
+++ 03-March/03-27-2022.js
@@ -140,3 +140,25 @@ function defineSuit(card) {
   }
 
 //*****I struggled with this solely because i couldn't determine how to produce the characters to test. I am frustrated as I could have completed more than i did and attempted to find the symbols later. LESSON LEARNED!!!!
+
+
+//=============================================================================================================
+
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+// Ex: 
+//     (Input1, Input2 -->Output)
+//     "4",  "5" --> "9"
+//     "34", "5" --> "39"
+//     "", "" --> "0"
+//     "2", "" --> "2"
+//     "-5", "3" --> "-2"
+// Notes: 
+//     If either input is an empty string, consider it as zero.
+//     Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+function sumStr(a,b) {
+    return (Number(a) + Number(b)).toString()
+}
+
+//*****This was a quick Kata to complete. I enjoyed running through it.
