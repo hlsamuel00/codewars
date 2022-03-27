e9504c4af1aa9ceb455c985f1f547dab4897aa36
Harvey S <hlsamuel00@yahoo.com>
2022-03-27 13:02:01 -0400

    Update 03-27-2022.js

--- 03-March/03-27-2022.js
+++ 03-March/03-27-2022.js
@@ -32,3 +32,22 @@ function between(a, b) {
 //=============================================================================================================
 
 
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+function countSheep(num){
+    let concat = ""
+    for (let i = 1; i <= num; i++){
+      concat += `${i} sheep...`
+    }
+     return concat
+  }
+
+//*****This was a little challenging, but I was able to get the answer worked out.
+
+
+//=============================================================================================================
+
+
