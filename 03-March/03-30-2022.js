cd4db9773a1ade31a8e34b92e17cbecd4b9a0a57
Harvey S <hlsamuel00@yahoo.com>
2022-03-30 11:52:00 -0400

    Create 03-30-2022.js

---
+++ 03-March/03-30-2022.js
@@ -0,0 +1,20 @@
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+String.prototype.toAlternatingCase = function () {
+    let alternatingCase = this.split('').map(x => x === x.toUpperCase() ? x.toLowerCase() : x.toUpperCase())
+    return alternatingCase.join('')
+}
+
+//*****I had a lot of fun building this out!! I excited that i build this that adds a method to a native object. This blew my mind building and I used the previous object classwork to follow the syntax, but i was able to deduce how to document what i wanted, just needed to get the syntax correct.  
