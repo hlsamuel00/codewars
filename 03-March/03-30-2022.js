dfb7125f0919e4f944afa9df1c6d5e358c221b10
Harvey S <hlsamuel00@yahoo.com>
2022-03-30 15:27:29 -0400

    Update 03-30-2022.js

--- 03-March/03-30-2022.js
+++ 03-March/03-30-2022.js
@@ -37,3 +37,37 @@ const addExtra = arr => arr.concat((arr[arr.length-1] + 1))
 
 
 //=============================================================================================================
+
+
+// Your Job:
+// Find the sum of all multiples of n below m
+
+// Keep in Mind:
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+
+//     Ex:
+//     sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+//     sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+//     sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+//     sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n,m){
+    let sum = 0
+    if (m > 0){
+      for (let i = 1; i < m; i++){
+        if (i % n === 0){
+          sum += i
+        }
+      }
+    } else {
+      return "INVALID"
+    }
+    return sum
+  }
+//*****I was able to get this Kata on the first try!! i was so ecstatic to get this. it had multiple parts, but due to me breaking it down in my head, i was able to see all parts and beast it!!!
+
+
+//=============================================================================================================
+
+
