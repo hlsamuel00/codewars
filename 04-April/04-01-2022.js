27f2f6210f69f77076bb87459acec102343b05e0
Harvey S <hlsamuel00@yahoo.com>
2022-04-01 11:51:01 -0400

    Create 04-01-2022.js

---
+++ 04-April/04-01-2022.js
@@ -0,0 +1,9 @@
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+const removeChar = str => str.slice(1,-1)
+
+//*****I completed this kata with ease. I ended up writing it and refactoring it before submitting. I am really proud of this. This is the first Kata of the new chapter that I'm starting. I am happy to be in this journey and working towards my purpose. I am in alignment and feel the universe conspiring to assist me with reaching my goals. 
+
+//=============================================================================================================
+
+
