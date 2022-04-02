bebc6ed41bd3782b0ba48efd6971bbeba25103e7
Harvey S <hlsamuel00@yahoo.com>
2022-04-02 17:04:34 -0400

    i added a new code wars challenge to 04-02-2022.js

--- 04-April/04-02-2022.js
+++ 04-April/04-02-2022.js
@@ -8,3 +8,33 @@
 const abbrevName = name => name.split(' ').map(x => x.slice(0,1)).join('.').toUpperCase()
 
 //*****I was able to complete this kata relatively easily. 
+
+
+//=============================================================================================================
+
+
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+const findNeedle = haystack => {
+    if (haystack == undefined){
+      return "Your function didn't return anything"
+    } else {
+      let p = haystack.findIndex(x => x == 'needle')
+      return `found the needle at position ${p}`
+    }
+  }
+
+//*****I completed this with a quick MDN review of the findIndex() method. I am curious to see how the code can be refactored. I refactored the code to:
+
+// const findNeedle = haystack => haystack == undefined ? "Your function didn't return anything" :
+//                    'found the needle at position ' + haystack.findIndex(x => x == 'needle')
+  
