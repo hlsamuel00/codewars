12adff21aa155c61ff28e978bcb2abef5449dc74
Harvey S <hlsamuel00@yahoo.com>
2022-04-03 12:00:43 -0400

    I added a new coding challenge to 04-03-2022.js

--- 04-April/04-03-2022.js
+++ 04-April/04-03-2022.js
@@ -15,3 +15,29 @@ const reverseSeq = n => {
 //const reverseSequence = n => Array(n).fill(0).map((e,i) => n - i)
 //*****This creates an array using the Array constructor and sets it's length to the number provided in the function, fills all values with 0, then maps the new values counting down from the number provided to being greater than 0. 
 
+
+//=============================================================================================================
+
+
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// Your function would return the following array:
+
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(x => !geese.includes(x))
+  };
+
+//*****I was able to fully understand why this works!! This was a valuable learning lesson!!
+
+
+//=============================================================================================================
