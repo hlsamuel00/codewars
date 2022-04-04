e3fd017a75eaab091047964a0507bbd9888abd4b
Harvey S <hlsamuel00@yahoo.com>
2022-04-04 15:35:45 -0400

    add new kata challenge to 04-04-2022.js

--- 04-April/04-04-2022.js
+++ 04-April/04-04-2022.js
@@ -90,4 +90,22 @@ const uniTotal = string => string.split('').reduce((acc, x, i) => acc += string.
 //=============================================================================================================
 
 
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+
+const DNAtoRNA = dna => dna.split('').map(x => x == "T" ? "U" : x).join('')
+
+//*****string manipulation again. I am getting better with this, but it's also challenging me to get better with array methods as well!
+
+
+//=============================================================================================================
 
