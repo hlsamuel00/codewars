30d98ff57b636e33f5c6ad3706494b7d1f255d4a
Harvey S <hlsamuel00@yahoo.com>
2022-04-05 10:45:04 -0400

    add new file 04-05-2022.js with first coding challenge

---
+++ 04-April/04-05-2022.js
@@ -0,0 +1,10 @@
+// Write a function which converts the input string to uppercase.
+
+const makeUpperCase = str => str.toUpperCase()
+
+//*****This was a breeze to complete. I am noticing how it's much easier to reall available methods for objects. 
+
+
+//=============================================================================================================
+
+
