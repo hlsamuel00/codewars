7bc445edb9dbfc31b0211c2bb9f33edceb979996
Harvey S <hlsamuel00@yahoo.com>
2022-04-07 12:24:25 -0400

    add new kata challenge to 04-07-2022.js

--- 04-April/04-07-2022.js
+++ 04-April/04-07-2022.js
@@ -15,3 +15,24 @@ const stringClean = (s) =>  s.split('').filter(x => !Array(..."1234567890").incl
 //=============================================================================================================
 
 
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. There is no need to check for negative values or values greater than 100.
+
+
+const getGrade = (s1, s2, s3) => {
+    let avg = Math.floor((s1 + s2 + s3)/3)
+    return avg > 89 ? 'A' : avg > 79 ? 'B' : avg > 69 ? 'C' : avg > 59 ? 'D' : 'F'
+}
+
+//*****I was able to ge through this with ease. I feel the creating of the variable was unnecessary, but assisted with the flow of the code. 
+
+
+//=============================================================================================================
