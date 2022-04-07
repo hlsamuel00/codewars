489a4204f79ba44321c2327e3f5e19dd0434529c
Harvey S <hlsamuel00@yahoo.com>
2022-04-07 14:12:08 -0400

    add new kata challenge to 04-07-2022.js

--- 04-April/04-07-2022.js
+++ 04-April/04-07-2022.js
@@ -60,3 +60,21 @@ const enough = (cap, on, wait) => cap - on - wait >= 0 ? 0 : Math.abs(cap - on -
 
 //=============================================================================================================
 
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+// Waiting for translations and Feedback! Thanks!
+
+const paperwork = (n, m) => n < 0 || m < 0 ? 0 : n * m
+
+//*****I was able to complete this kata on the first try. 
+
+
+//=============================================================================================================
+
+
