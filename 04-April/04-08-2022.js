ef7c6b0a43cca0672915b15a7437164a585e6a65
Harvey S <hlsamuel00@yahoo.com>
2022-04-08 10:34:09 -0400

    add new kata challenge to 04-08-2022.js

--- 04-April/04-08-2022.js
+++ 04-April/04-08-2022.js
@@ -31,3 +31,17 @@ function magNumber(info){
 //=============================================================================================================
 
 
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - sonYearsOld * 2)
+
+//*****Working through the math of this was the tricky part, but i was able to get it figured out!!
+
+
+//=============================================================================================================
+
+
