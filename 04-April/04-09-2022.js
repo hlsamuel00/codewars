8986676fd3d58f006cee0dd03876e6614f595d74
Harvey S <hlsamuel00@yahoo.com>
2022-04-09 14:53:50 -0400

    add new kata challenge to 04-09-2022.js

--- 04-April/04-09-2022.js
+++ 04-April/04-09-2022.js
@@ -125,3 +125,31 @@ const capitalizeWord = word => word[0].toUpperCase() + word.slice(1)
 
 
 //=============================================================================================================
+
+
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nbPetals) {
+    let petals = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+    while(nbPetals > petals.length){
+      nbPetals -= petals.length
+    }
+    return petals[nbPetals-1]
+  }
+
+//*****I looked through the solutions and devised this refactored code:
+
+            // const howMuchILoveYou = nbPetals => {
+            //     let petals = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+            //     return petals[(nbPetals-1) % petals.length]
+            // }
