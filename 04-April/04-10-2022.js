5619be12a66a37e29a322d2d3b8b63d890c83942
Harvey S <hlsamuel00@yahoo.com>
2022-04-10 14:18:58 -0400

    add new file 04-10-2022.js with new kata challenge

---
+++ 04-April/04-10-2022.js
@@ -0,0 +1,55 @@
+//========Today I will be attempting 7kyu problems!!!========//
+
+// Create a function that takes an array of letters, and combines them into words in a sentence.
+
+// The array will be formatted as so:
+
+// [['J','L','L','M']
+// ,['u','i','i','a']
+// ,['s','v','f','n']
+// ,['t','e','e','']]
+// The function should combine all the 0th indexed letters to create the word 'just', all the 1st indexed letters to create the word 'live', etc.
+
+// Shorter words will have an empty string in the place once the word has already been mapped out (see the last element in the last element in the array).
+
+// Examples:
+
+//     arrAdder([
+//     ['J','L','L','M'],
+//     ['u','i','i','a'],
+//     ['s','v','f','n'],
+//     ['t','e','e','']
+//     ]) // => "Just Live Life Man"
+
+//     arrAdder([ 
+//       [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+//       [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+//       [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+//       [ '', 'o', '', '', 'e', '', '', 'l' ],
+//       [ '', 'c', '', '', 'r', '', '', '' ],
+//       [ '', 'h', '', '', 'h', '', '', '' ],
+//       [ '', 'o', '', '', 'o', '', '', '' ],
+//       [ '', 'n', '', '', 'u', '', '', '' ],
+//       [ '', 'd', '', '', 's', '', '', '' ],
+//       [ '', 'r', '', '', 'e', '', '', '' ],
+//       [ '', 'i', '', '', '', '', '', '' ],
+//       [ '', 'a', '', '', '', '', '', '' ] ]) // => "The Mitochondria is the powerhouse of the cell"
+
+const arrAdder = arr => {
+    let arr2 = []
+    for (let i = 0; i < arr[0].length;){
+      let arr3 =[]
+      for (let j = 0; j < arr.length; j++){
+        if (arr[j][i] != ''){
+          arr3.push(arr[j][i])
+          }
+      }
+      arr2.push(arr3.join(''))
+      i++
+    }
+}
+
+//*****I looked through the solutions and found the code below:
+    const arrAdd = arr => arr[0].map((_,i) => arr.map((_,j) => arr[j][i]).join('')).join(' ')
+//*****I still can't quite figure out how it omits the blank/undefined cells in the arrays.
+
