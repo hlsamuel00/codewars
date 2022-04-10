42a7102206b21153ba550629b3fc63f4b4df39cc
Harvey S <hlsamuel00@yahoo.com>
2022-04-10 19:27:08 -0400

    add new kata challenge to 04-10-2022.js

--- 04-April/04-10-2022.js
+++ 04-April/04-10-2022.js
@@ -53,3 +53,33 @@ const arrAdder = arr => {
     const arrAdd = arr => arr[0].map((_,i) => arr.map((_,j) => arr[j][i]).join('')).join(' ')
 //*****I still can't quite figure out how it omits the blank/undefined cells in the arrays.
 
+
+//=============================================================================================================
+
+
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+// Example:
+//     a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+//     a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+//     mxdiflg(a1, a2) --> 13
+
+// Bash note:
+//     input : 2 strings with substrings separated by ,
+//     output: number as a string
+
+
+function mxdiflg(a1, a2) {
+    if (a1.length == 0 || a2.length ==0) return -1 
+    let xMax = Math.max(...a1.map(x=>x.length))
+    let yMax = Math.max(...a2.map(x=>x.length))
+    let xMin = Math.min(...a1.map(x=>x.length))
+    let yMin = Math.min(...a2.map(x=>x.length))
+    return Math.max(Math.abs(xMax-yMin) , Math.abs(yMax-xMin))
+  }
+
+//*****I was able to get this kata from looking at a solution. Although the breakdown makes sense, i don't understand it under the parameters set for the problem. 
