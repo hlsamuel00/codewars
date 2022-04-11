76b78210e7792ca96d905d0b0f385e7d07714f82
Harvey S <hlsamuel00@yahoo.com>
2022-04-11 12:06:53 -0400

    add new kata challenge to 04-11-2022.js

--- 04-April/04-11-2022.js
+++ 04-April/04-11-2022.js
@@ -15,3 +15,83 @@ const betterThanAverage = (classPoints, yourPoints)=> yourPoints > ((classPoints
 
 
 //=============================================================================================================
+
+//*********7kyu problem*********//
+
+// #'x' marks the spot
+
+// ##Task:
+
+// Given a two dimensional array, return the co-ordinates of 'x'.
+
+// If 'x' is not inside the array, or if 'x' appears multiple times, return []
+
+// The co-ordinates should be zero indexed.
+
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+// Example test cases:
+
+// 'Return an empty array if input is an empty array' => []
+
+// [] 
+
+// 'Return an empty array if no x found' => []
+
+// [
+//   ['o', 'o'],
+//   ['o', 'o']
+// ]
+
+// 'Return an empty array if more than one x found' => []
+
+// [
+//   ['x', 'o'],
+//   ['o', 'x']
+// ]
+
+// 'Return [0,0] when x at top left' => [0, 0]
+
+// [
+//   ['x', 'o'],
+//   ['o', 'o']
+// ]
+
+// 'Return [4,6] for the example below' => [4, 6]
+
+// [
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+// ]
+
+
+const xMarksTheSpot = input =>{
+    let coords = input.map((x,i) => x.map((y,j) => [i,j])).reduce((a,b) => a.concat(b), []).filter(([i, j]) => input[i][j] == 'x')
+    return coords.length === 1 ? coords[0] : []
+}
+
+
+//******I wasn't able to get the kata on my own, but I located the solutions and found one that i could recreate:
+    // const xMarksTheSpot = (input) => {
+        // let coords = []
+    // 
+        // for (let i = 0; i < input.length; i++) {
+        //   if (input[i].includes("x")) {
+            // coords.push([i, input[i].indexOf("x")])
+        //   }
+        // }
+        // if (coords.length === 1) {
+        //   return coords[0]
+        // } else {
+        //   return []
+        // }
+    //   }
+
+
+//=============================================================================================================
+
+
