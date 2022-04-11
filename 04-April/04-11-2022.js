ebf94c02938f86af007acf0cfdb894457c434ef1
Harvey S <hlsamuel00@yahoo.com>
2022-04-11 12:20:30 -0400

    add new kata challenge to 04-11-2022.js

--- 04-April/04-11-2022.js
+++ 04-April/04-11-2022.js
@@ -95,3 +95,25 @@ const xMarksTheSpot = input =>{
 //=============================================================================================================
 
 
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+// Please use the following function names:
+// addition = add
+// multiply = multiply
+// division = divide (both integer and float divisions are accepted)
+// modulus = mod
+// exponential = exponent
+// subtraction = subt
+    // Note: All math operations will be: a (operation) b
+
+const add = (a,b) => a + b;
+const divide = (a,b) => a / b;
+const multiply = (a,b) => a * b;
+const mod = (a,b) => a % b;
+const exponent = (a,b) => a**b ;
+const subt = (a,b) => a - b;
+
+//*****I was able to get these relatively easily. I completed these on the first try,but went back and wrote them as arrow functions before submitting. 
+
+
+//=============================================================================================================
+
