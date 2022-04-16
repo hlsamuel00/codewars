14c39578f8caf5e33fd1d03b79455af58934d019
Harvey S <hlsamuel00@yahoo.com>
2022-04-16 16:44:21 -0400

    add new kata challenge to file 04-15-2016.js and add new file 04-16-2022.js with new kata challenge

--- 04-April/04-15-2022.js
+++ 04-April/04-15-2022.js
@@ -15,3 +15,24 @@
 const isDivideBy = (number, a, b) => number % a == 0 && number % b == 0
 
 //*****I was able to complete this kata with ease. 
+
+
+//=============================================================================================================
+
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+
+const century = (year) => Math.ceil(year / 100)
+
+//*****I was able to breeze through this and came up with Math.ceil() to get it to round up. I was proud to have thought if it instantly.
