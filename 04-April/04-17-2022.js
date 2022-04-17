4927ae5d30ee200c12951dd4a7a9b3cbf7df23c8
Harvey S <hlsamuel00@yahoo.com>
2022-04-17 01:57:13 -0400

    add new kata challenge to 04-17-2022.js

--- 04-April/04-17-2022.js
+++ 04-April/04-17-2022.js
@@ -15,4 +15,28 @@ const solution = nums => nums == null ? [] : nums.sort((el, el2) => el - el2)
 //=============================================================================================================
 
 
+//     *****7KYU*****
 
+// Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+// Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+// solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+// See test cases for more examples.
+
+// Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+// Good luck!
+
+function solve(arr){  
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return arr.map(x => {
+     return x.split('').map((y,j) => j+1 == (alphabet.indexOf(y.toLowerCase())+1) ? 1 : 0).reduce((acc,el) => acc+=el,0)
+     })
+};
+
+//*****I solved this one with a bit of time!! I was able to refactor my code from looking at some of the solutions and came up with this code:
+        function solve(arr){  
+            let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+            return arr.map(x => x.split('').filter((y,j) => j == (alphabet.indexOf(y.toLowerCase()))).length
+        )};
