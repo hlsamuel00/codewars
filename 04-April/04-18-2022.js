7453a8041b1f9312b619b0cd816071fba6fd8331
Harvey S <hlsamuel00@yahoo.com>
2022-04-18 22:39:57 -0400

    add new kata challenge to 04-18-2022

--- 04-April/04-18-2022.js
+++ 04-April/04-18-2022.js
@@ -45,3 +45,11 @@ const past = (h, m, s) =>{
 
 
 //=============================================================================================================
+
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+const simpleMultiplication = number => number % 2 == 0 ? number * 8 : number * 9
+
+
+//*****I got this kata on the first try!!!!!
