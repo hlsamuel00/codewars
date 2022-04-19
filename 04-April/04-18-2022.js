fa73713adf1dafd7efbd4ed155c11d66f26d3ff4
Harvey S <hlsamuel00@yahoo.com>
2022-04-18 21:54:38 -0400

    add new file 04-18-2022.js with new kata challenge

---
+++ 04-April/04-18-2022.js
@@ -0,0 +1,14 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+const otherAngle = (a, b) => 180 - a - b
+
+//*****This kata was easy to tackle. No complaints and no issues faced.
+
+
+//=============================================================================================================
+
+
