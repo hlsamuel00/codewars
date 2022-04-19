560af64b598055564b997c5e06b15178a098111b
Harvey S <hlsamuel00@yahoo.com>
2022-04-18 22:23:46 -0400

    add new kata challenge to 04-18-2022.js

--- 04-April/04-18-2022.js
+++ 04-April/04-18-2022.js
@@ -12,3 +12,36 @@ const otherAngle = (a, b) => 180 - a - b
 //=============================================================================================================
 
 
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+const past = (h, m, s) =>{
+    //testing for invalid inputs
+  if (h < 0 || m < 0 || s < 0){
+    return 'Invalid Input'
+    //increment the minutes and hours for values over the threshold
+  } else if (s > 59){ 
+    s = s % 60
+    m+=1
+  } else if(m > 59){
+    m = m % 60
+    h+=1
+  }
+  return ((h*3600) + (m*60) + (s)) * 1000
+}
+
+
+//=============================================================================================================
