465d684846cb4b9261b4aa367d75c1d543521982
Harvey S <hlsamuel00@yahoo.com>
2022-04-20 16:15:55 -0400

    add new kata challenge to 04-20-2022.js

--- 04-April/04-20-2022.js
+++ 04-April/04-20-2022.js
@@ -80,3 +80,25 @@ function solve(s){
 //*****I was able to resolve this using regex. I struggled a bit with combining verification with regex, but have figured out how to refactor the code below from looking at some solutions:
 
     const solve = s => Array(/[A-Z]/g , /[a-z]/g, /\d/g, /\W/g).map(x => s.match(x).length || 0)
+
+
+//=============================================================================================================
+
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+const makeNegative = num => num <= 0 ? num : -num
+
+//*****I was able to complete this kata very easily.
+
+
+//=============================================================================================================
