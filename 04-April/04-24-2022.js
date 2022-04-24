16a20420eea3d2a02c9db95a889806b45c488900
Harvey S <hlsamuel00@yahoo.com>
2022-04-24 12:05:12 -0400

    add new kata challenge to 04-24-2022.js

--- 04-April/04-24-2022.js
+++ 04-April/04-24-2022.js
@@ -51,3 +51,35 @@ const expressionMatter = (a, b, c) => {
 const litres = time => Math.floor( time * .5)
 
 //*****I was able to blow through this kata in about 2 mins.
+
+
+//=============================================================================================================
+
+
+// Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as it's input). Return the final value after execution is complete.
+
+// function add(num) {
+//   return num + 1
+// }
+
+// function mult(num) {
+//   return num * 30
+// }
+
+// chain(2, [add, mult]);
+// returns 90;
+
+
+const chain = (input, fs) => {
+    const add = number => number + 10
+    const mult = number => number * 30
+    return mult(add(input))
+  }
+
+
+//*****I wasn't able to get this kata fully, but located a solution that made the most sense for me:
+    //const chain = (input, fs) => fs.reduce((acc,x) => x(acc), input);
+//*****I fully understand how to iterate through the function using the reduce method and how it takes my value and enters it into the next function seamlessly
