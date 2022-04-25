4ba4f44a491f874836806b40ab83012cc1c21330
Harvey S <hlsamuel00@yahoo.com>
2022-04-25 11:01:38 -0400

    add new kata challenge to 04-25-2022.js

--- 04-April/04-25-2022.js
+++ 04-April/04-25-2022.js
@@ -1,3 +1,5 @@
+    //======7kyu======
+
 // Given two numbers x and n, calculate the (positive) nth root of x; this means that being r = result, r^n = x
 
 // Examples
@@ -14,3 +16,30 @@
 const root = (x, n) => Math.pow(x,1/n)
 
 //*****I was able to get this kata fairly easily. I recalled from math where rooting a number is the same as a fractional exponent after a quick glance at stack overflow. 
+
+
+//=============================================================================================================
+
+
+    //======6kyu======
+
+    // You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+    // Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+    
+    // []                                -->  "no one likes this"
+    // ["Peter"]                         -->  "Peter likes this"
+    // ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+    // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+    // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+    // Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+    const likes = names => {
+        return names.length == 0 ? 'no one likes this' : 
+        names.length == 1 ? names.join('').concat(' likes this') : 
+        names.length == 2 ? names.join(' and ').concat(' like this') :
+        names.length == 3 ? names[0].concat(', ') + names.slice(1).join(' and ').concat(' like this') : 
+        names[0].concat(', ') + names[1] + ` and ${names.length-2} others like this`
+      }
+    
+    //*****I am extremely proud of being able to tackle this one with little difficulty!! I had to restart the code after a few failed attempts, but breaking it down by each case helped me get it resolved. The other solutions look very similar to my solution
