b884c98d04f7e22f6f0c05db46ad30229e3dcee9
Harvey S <hlsamuel00@yahoo.com>
2022-04-26 09:43:09 -0400

    add new file 04-26-2022.js with new kata challenge

---
+++ 04-April/04-26-2022.js
@@ -0,0 +1,10 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+const square = num => num**2
+
+//*****I was able to get this kata very easily. 
+
+
+//=============================================================================================================
+
+
