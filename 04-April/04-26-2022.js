e7142ea70181f69090c45346b59191a7e3162dd4
Harvey S <hlsamuel00@yahoo.com>
2022-04-26 11:12:22 -0400

    add new kata challenge to 04-26-2022.js

--- 04-April/04-26-2022.js
+++ 04-April/04-26-2022.js
@@ -8,3 +8,22 @@ const square = num => num**2
 //=============================================================================================================
 
 
+// Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+// Example:
+
+// [12, 34, 56, 78]  =>  "09"
+// [2015, 8, 26]     =>  "3479"
+// Note: the digits in the resulting string should be sorted.
+
+
+const unusedDigits = (...args) => Array(...'0123456789').filter(x => !Array.from(args).join('').split('').includes(x)).join('')
+
+//*****I have heavy frustration around this kata. I had figured this code out, but due to not having the spread operator in the arguments, my code did not produce the adequate result. A look at the solutions let me know that i had gotten it, just needed a bit of help to get it to work. i am extremely proud of the level of comprehension I had. The code above is my refactored code, below is the original code I wrote:
+    const unusedDigits = (...args) => {
+        let nums = Array(...'0123456789')
+        let arg = Array.from(args).join('').split('')
+        return nums.filter(x => !arg.includes(x)).join('')
+    }
+
+    
