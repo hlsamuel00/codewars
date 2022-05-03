ca5aa74ca4ef310f86f4e53d02d8c95e3f6be21c
Harvey S <hlsamuel00@yahoo.com>
2022-05-02 21:50:46 -0400

    add new kata challenge to 05-02-2022.js

--- 05-May/05-02-2022.js
+++ 05-May/05-02-2022.js
@@ -11,3 +11,14 @@ const hoopCount = (n) => n > 9 ? "Great, now move on to tricks" : "Keep at it un
 
 
 //=============================================================================================================
+
+
+    //======7kyu======
+
+// For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+// Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array). Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+
+const bingo = (a) => Array(2,7,9,14,15).filter(x => !a.includes(x)).length == 0 ? 'WIN' : 'LOSE'
+
+//*****This one was a bit trickier to figure out, but I was able to get it through some deep thinking getting my daughter. Looking through the solutions, versus using filter(), they used every() which does the same as filter, but doesn't return a new array which saved me from the .length property.
