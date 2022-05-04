0019f88ca0a01aca8bd6befcf3d105ce3a1c9776
Harvey S <hlsamuel00@yahoo.com>
2022-05-04 12:20:12 -0400

    add new kata challenge to 05-04-2022.js

--- 05-May/05-04-2022.js
+++ 05-May/05-04-2022.js
@@ -28,3 +28,27 @@ const zeroAndOne = (s) => s.replace( /(10|01)/g, '').length
 //=============================================================================================================
 
 
+// You are given 5 variables;
+
+// sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+// sharkSpeed = how fast it can move in metres/second.
+
+// pontoonDistance = how far you need to swim to safety in metres.
+
+// youSpeed = how fast you can swim in metres/second.
+
+// dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+// The pontoon, you, and the shark are all aligned in one dimension.
+
+// If you make it, return "Alive!", if not, return "Shark Bait!".
+
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    if (dolphin){
+      sharkSpeed/=2
+    }
+    return sharkDistance / sharkSpeed < pontoonDistance / youSpeed ? 'Shark Bait!' : 'Alive!'
+  }
+  
