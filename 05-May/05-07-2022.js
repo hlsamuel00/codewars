d7d935a4fbb7b0439da085da57b98862843cfcb2
Harvey S <hlsamuel00@yahoo.com>
2022-05-07 18:36:31 -0400

    add new ladder challenge to 05-07-2022.js

--- 05-May/05-07-2022.js
+++ 05-May/05-07-2022.js
@@ -38,3 +38,61 @@ const dontGiveMeFive = (start, end) => Array.from({length:end-start+1}, (_,i) =>
 
 
 //=============================================================================================================
+
+
+    //======6kyu======
+
+    // Task:
+    // Complete the function piecesValue/pieces_value that accepts two arguments, an 8x8 array (arr),representing a chess board and a string (s). Depending on the value of the string s (which can be either "white" or "black") calculate the value of the pieces on the table for the corresponding player(white or black).
+    // Empty fields will be marked as a space " " while the fields with pieces look like this:
+    
+    // "w-king"   //meaning white king
+    // "b-bishop" //a black bishop
+    // "w-pawn"   //white pawn
+    // ...and so on. Preloaded for you there is an object called hash (values in python):
+    
+    // const hash = Object.freeze({
+    //   queen: 9, rook: 5, bishop: 3, knight: 3, pawn: 1
+    // });
+    // Having the estimated value of each piece. This is a rough estimation and the real piece value depends on other factors in game as well ,such as the game being a closed or open one, which can favor either knights or bishops. (If you are interested more about that here: open vs closed game.) But for our purposes we will use the mentioned values.
+    // Note: the value of a king cannot be estimated because without it the game would be over, so DO NOT take into consideration the value of the king.You will not be tested for invalid input.
+    // Example case:
+    // piecesValue([[' ',' ',' ',' ',' ',' ',' ',' '],
+    //              [' ',' ','b-queen',' ',' ',' ',' ','w-queen'],
+    //              [' ','b-king',' ',' ','w-rook',' ',' ',' '],
+    //              [' ',' ',' ',' ',' ',' ',' ',' '],
+    //              [' ',' ',' ',' ',' ',' ',' ',' '],
+    //              ['w-king',' ',' ',' ',' ',' ',' ',' '],
+    //              [' ',' ',' ',' ',' ',' ',' ',' '],
+    //              [' ',' ',' ',' ',' ',' ',' ',' ']], 
+    //              'white');
+    // //should be 14 since white has a queen and a rook 
+    // //while the same table would return 9 for 'black'
+    
+    // piecesValue([[' ',' ',' ',' ',' ',' ',' ',' '],
+    //              [' ',' ','b-queen',' ',' ',' ',' ','w-queen'],
+    //              [' ','b-king',' ','b-pawn','w-rook',' ',' ',' '],
+    //              [' ',' ',' ',' ','w-pawn',' ',' ',' '],
+    //              [' ',' ',' ',' ',' ','w-bishop',' ',' '],
+    //              ['w-king',' ',' ',' ',' ',' ',' ',' '],
+    //              [' ',' ',' ','b-pawn',' ',' ',' ',' '],
+    //              [' ',' ',' ',' ',' ',' ',' ',' ']],
+    //              'white');
+    // //should return 18 for 'white' (queen, rook, pawn, bishop)
+    // //and 11 in case s is 'black'(queen, pawn, pawn)
+    // Happy coding warrior!
+
+    const piecesValue = (arr, s) => {
+        let c = s == 'white' ?'b' : 'w'
+        let pieces = arr.map(x => x.filter(x => x != false)) //removed the blank spaces to see pieces left
+        pieces = pieces.map(x => x.filter(x => x.split('-')[0] != c )) //determined the color of pieces to count  
+        pieces = pieces.map(x => x.map(x => x.slice(2))) //removes the piece color
+        pieces = pieces.map(x => x.map(x => parseInt(hash[x] || 0))) //replaces piece names with corresponding values
+        pieces = pieces.map(x => x.reduce((acc,el) => acc + el, 0)).reduce((acc,el) => acc + el, 0) // reduces into a single digit value
+        return pieces
+      }
+    
+//*****This definitely took me longer than 20 mins, but I am happy that I was able to get this resolved. Looking over the solutions, I was WAAAYY off, but can partially understand how they were able to get the solutions they got.
+
+
+//=============================================================================================================
