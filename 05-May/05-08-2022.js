248ee1e887f16f0ea5a05156e4ba88d5a127de91
Harvey S <hlsamuel00@yahoo.com>
2022-05-08 16:32:59 -0400

    add new kata challenge to 05-08-2022.js

--- 05-May/05-08-2022.js
+++ 05-May/05-08-2022.js
@@ -53,3 +53,29 @@ const stringMerge = (str1, str2, l) => str1.slice(0,str1.indexOf(l)) + str2.subs
 
 
 //=============================================================================================================
+
+
+    //======7kyu======
+
+// Given a random string consisting of numbers, letters, symbols, you need to sum up the numbers in the string.
+
+// Note:
+
+// Consecutive integers should be treated as a single number. eg, 2015 should be treated as a single number 2015, NOT four numbers
+// All the numbers should be treaded as positive integer. eg, 11-14 should be treated as two numbers 11 and 14. Same as 3.14, should be treated as two numbers 3 and 14
+// If no number was given in the string, it should return 0
+// Example:
+
+// str = "In 2015, I want to know how much does iPhone 6+ cost?"
+// The numbers are 2015, 6
+
+// Sum is 2021.
+
+
+const sumFromString = (str) => !!str.match(/(\d)+/g) ? str.match(/(\d)+/g).reduce((acc,el) => acc + parseInt(el),0) : 0
+
+//*****I was able to get this kata fairly easily, but i did review the MDN a bit for regex. I also reviewed the solutions and located a way to refactor my code below:
+
+const sumFromString2 = (str)=> str.split(/\D+/g).reduce((acc,el) => +acc + +el) || 0
+
+//*****I was extremely proud of my progress with this one!!
