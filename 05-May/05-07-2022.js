d135ec537b3a57976529edae22410fcf6fcdc1ed
Harvey S <hlsamuel00@yahoo.com>
2022-05-07 21:53:17 -0400

    add new ladder challenge to 05-07-2022.js

--- 05-May/05-07-2022.js
+++ 05-May/05-07-2022.js
@@ -96,3 +96,25 @@ const dontGiveMeFive = (start, end) => Array.from({length:end-start+1}, (_,i) =>
 
 
 //=============================================================================================================
+
+
+    //======7kyu======
+
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+  }
+
+  //*****I did not get this kata because i attempted to write this as an arrow function, versus the standard block notation. Due to that; I will be redoing the 7kyu ladder step.
+
+
+//=============================================================================================================
+
