3211d5c4b5f16e337cdaf9eb0290d6a9d33b5ede
Harvey S <hlsamuel00@yahoo.com>
2022-05-08 16:00:53 -0400

    add new kata challenge to 05-08-2022.js

--- 05-May/05-08-2022.js
+++ 05-May/05-08-2022.js
@@ -35,3 +35,21 @@ const dropCap = n => n.split(' ').map(x => x.length > 2 ? x[0].toUpperCase() + x
 
 
 //=============================================================================================================
+
+
+    //======7kyu======
+
+// Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+// Examples
+// ("hello", "world", "l")       ==>  "held"
+// ("coding", "anywhere", "n")   ==>  "codinywhere"
+// ("jason", "samson", "s")      ==>  "jasamson"
+// ("wonderful", "people", "e")  ==>  "wondeople"
+
+const stringMerge = (str1, str2, l) => str1.slice(0,str1.indexOf(l)) + str2.substring(str2.indexOf(l))
+
+//*****I was able to get through this challenge with a quick refresher on the MDN.
+
+
+//=============================================================================================================
