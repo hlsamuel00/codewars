849b76550ecb82a46c1538f9dbbd09590722cfc9
Harvey S <hlsamuel00@yahoo.com>
2022-05-08 15:46:02 -0400

    add new kata challenge to 05-08-2022.js

--- 05-May/05-08-2022.js
+++ 05-May/05-08-2022.js
@@ -15,3 +15,23 @@ const longest = words => Math.max(...words.map(x => x.length))
 //=============================================================================================================
 
 
+    //======7kyu======
+
+// DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+
+// But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+// *should work also on Leading and Trailing Spaces and caps.
+
+// "apple"            => "Apple"
+// "apple of banana"  => "Apple of Banana"
+// "one   space"      => "One   Space 
+// "   space WALK   " => "   Space Walk   " 
+// Note: you will be provided atleast one word and should take string as input and return string as output.
+
+const dropCap = n => n.split(' ').map(x => x.length > 2 ? x[0].toUpperCase() + x.slice(1).toLowerCase() : x).join(' ')
+
+//*****I was able to get this kata very easily as well! I am truly noticing my progress!
+
+
+//=============================================================================================================
