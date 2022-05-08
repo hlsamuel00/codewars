5c4143c21a03ae1560cb198e2e778bca10fbf8d9
Harvey S <hlsamuel00@yahoo.com>
2022-05-07 22:54:35 -0400

    add final leg of ladder challenge to 05-07-2022.js

--- 05-May/05-07-2022.js
+++ 05-May/05-07-2022.js
@@ -140,3 +140,28 @@ const isAllPossibilities2 = x => x.every((_,i) => x.includes(i)) && !!x.length
 
 
 //=============================================================================================================
+
+ 
+    //***8kyu***
+
+// This is a spin off of my first kata.
+
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+// Examples
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+const array = arr => arr.split(',').length > 2 ? arr.split(',').slice(1,-1).join(' ') : null
+
+
+//*****This was very easy to complete. I had a slight stumble, but I was able to work through it!! I have completed the Array Ladder challenge!!!
