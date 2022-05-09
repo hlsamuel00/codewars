57a3fd56905faf42051d8e2af228ae1517964dc1
Harvey S <hlsamuel00@yahoo.com>
2022-05-09 12:49:39 -0400

    add new kata challenge to 05-09-2022.js

--- 05-May/05-09-2022.js
+++ 05-May/05-09-2022.js
@@ -31,3 +31,22 @@ const potatoes = (p0, w0, p1) => parseInt(w0 * ((100-p0) / (100-p1)))
 
 
 //=============================================================================================================
+
+
+    //======7kyu======
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+const addBinary = (a,b) => (a+b).toString(2)
+
+//*****I was able to get thiskata farily easily, but used the MDN to understand that parseInt(2) turns binary into a number, where .toString(2) converts a number to a binary string.
+
+
+//=============================================================================================================
