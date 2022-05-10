8dbf0e4f624d4d06eccba51c7261ebcd66f947f4
Harvey S <hlsamuel00@yahoo.com>
2022-05-09 20:12:49 -0400

    add final challenge of the day to 05-09-2022.js

--- 05-May/05-09-2022.js
+++ 05-May/05-09-2022.js
@@ -120,3 +120,32 @@ const toBinary = n => (n>>>0).toString(2)
 
 
 //=============================================================================================================
+
+
+    //======7kyu======
+
+// A parity bit, or check bit, is a bit added to a string of bits to ensure that the total number of 1-bits in the string is even or odd. Parity bits are used as the simplest form of error detecting code.
+
+// You have two parameters, one being the wanted parity (always 'even' or 'odd'), and the other being the binary representation of the number you want to check.
+
+// Your task is to return an integer (0 or 1), whose parity bit you need to add to the binary representation so that the parity of the resulting string is as expected.
+
+// Example:
+
+//     Parity: 'even'
+//     Bin: '0101010'
+//     Result: 1
+// Because there is an odd number of 1-bits (3) you need to put another 1 to it to get an even number of 1-bits.
+
+// For more information: https://en.wikipedia.org/wiki/Parity_bit
+
+function checkParity(parity, bin){
+    return !([...bin].filter(x => x == 1).length % 2) && parity == 'even' ? 0 :
+             [...bin].filter(x => x == 1).length % 2 && parity == 'odd' ? 0 : 1
+}
+
+//*****I was able to get this kata after some deep thought, but also located how to refactor the code below from the solutions:
+
+const checkParity = (parity, bin) => +(parity == (bin.match(/1/g).length % 2 ? 'even': 'odd'))
+
+//*****I am really proud of my progress and being able to understand how this broke down. 
