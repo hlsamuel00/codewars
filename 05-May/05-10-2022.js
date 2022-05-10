5d9c0765eedba5cc21cafdb33b1ceb1501bc1f94
Harvey S <hlsamuel00@yahoo.com>
2022-05-10 15:18:48 -0400

    add new kata challenge to 05-10-2022.js

--- 05-May/05-10-2022.js
+++ 05-May/05-10-2022.js
@@ -222,3 +222,38 @@ class Ship{
 
 
 //=============================================================================================================
+
+
+// Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+// If the user tries to guess more than the limit, the function should throw an error.
+// If the user guess is right it should return true.
+// If the user guess is wrong it should return false and lose a life.
+// Can you finish the game so all the rules are met?
+
+class Guesser {
+    constructor(number, lives) {
+        this.number = number;
+        this.lives = lives;
+    }
+    
+    guess(n) {
+        if(!this.lives){
+            throw 'No lives left'
+        }
+
+        if(this.number == n){ 
+            return true
+        }
+
+        else{ 
+            this.lives-- 
+            return false
+        }
+    }
+}
+
+//*****I wasn't able to get this kata, but looking at the solutions, i was able to understand it and wrote the code above.
+
+
+//=============================================================================================================
