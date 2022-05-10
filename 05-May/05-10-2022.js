abd9d64c54e749330b2f0fc153946930cc61092d
Harvey S <hlsamuel00@yahoo.com>
2022-05-10 14:40:34 -0400

    add new kata challenge to 05-10-2022.js

--- 05-May/05-10-2022.js
+++ 05-May/05-10-2022.js
@@ -151,3 +151,29 @@ class Person{
 
 
 //=============================================================================================================
+
+
+// Classy Extensions
+
+// Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing extends keyword. You will be preloaded with the Animal class, so you should only edit the Cat class.
+
+// Task
+
+// Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+// The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+
+
+class Cat extends Animal {
+    constructor(name){
+      super(name)
+    }
+    speak(){
+      return `${this.name} meows.`
+    }
+}
+
+//*****This was very easy to breeze through, but this is outlining my progress with OOP.
+
+
+//=============================================================================================================
