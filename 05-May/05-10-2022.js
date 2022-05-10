c518cc4ff28cf01a2bae73f975bc311feebb8bce
Harvey S <hlsamuel00@yahoo.com>
2022-05-10 15:46:27 -0400

    add new kata challenge to 05-10-2022.js

--- 05-May/05-10-2022.js
+++ 05-May/05-10-2022.js
@@ -257,3 +257,32 @@ class Guesser {
 
 
 //=============================================================================================================
+
+
+// Hey Codewarrior!
+
+// You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+
+// Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+
+// The constructor taking no arguments should assign 0 to Cube's Side property.
+
+
+class Cube{
+    constructor(n) {
+      this._side = Math.abs(n) || 0
+    }
+    getSide() {
+      return this._side
+    }
+    setSide(n){
+      this._side = Math.abs(n)
+      return this._side
+    }
+}
+
+
+//*****I was able to get this kata challenge almost on the first try. I had an error with the setSide() method, but was able to figure it out timely and streamlined the code.
+
+
+//=============================================================================================================
