4e1fa7833d3d6a4388b91e18c5a5b06b38e13088
Harvey S <hlsamuel00@yahoo.com>
2022-05-17 16:34:06 -0400

    add new kata challenge to 05-17-2022.js

--- 05-May/05-17-2022.js
+++ 05-May/05-17-2022.js
@@ -21,3 +21,19 @@ class Person{
 
 
 //=============================================================================================================
+
+
+//P: input will be a string; write one line function (arrow function); if provided with empty string, integer, or an array, return 'Broken!'
+//R: the string will return the input string without a's or e's, or 'Broken!' if response is not a string
+//E: onePunch('Beard Knife Grenade Motorbike Hat'), 'Brd Grnd Ht Knif Motorbik')
+
+const onePunch = items => typeof items == 'string' && items.length ? items.split(' ').sort((el,el2) => el.localeCompare(el2)).map(x => x.split('').filter(x => x.toLowerCase() != 'e' && x.toLowerCase() != 'a').join('')).join(' ') : 'Broken!'
+//test for string
+//turn string into an array
+//sort array alphabetically
+//filter through array for e's and a's and remove them
+//join the array back to string
+
+//*****I was able to get this kata with a bit of time, but looking through the solutions, i was able to refactor my code below:
+
+const onePunch2 = items => typeof items == 'string' && items.length ? items.split(' ').sort().join(' ').replace(/[aeAE]/g,'') : 'Broken!'
