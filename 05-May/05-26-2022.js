de1423ca60abc01eaef79fc851841e0e8240f526
Harvey S <hlsamuel00@yahoo.com>
2022-05-26 10:51:18 -0400

    add new file 05-26-2022.js with new kata challenge

---
+++ 05-May/05-26-2022.js
@@ -0,0 +1,18 @@
+// Task
+// Please refer to two example above and write your first JS function.
+
+// mission 1:
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+// mission 2:
+// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+// mission 3:
+// type the console.log() in the next line(don't forget to put the str in the brackets).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+const helloWorld = () => {
+    let str = 'Hello World!'
+    console.log(str)
+}
