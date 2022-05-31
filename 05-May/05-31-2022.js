80ceb65b1f6d22040215ed8d71bf478c6df31ce1
Harvey S <hlsamuel00@yahoo.com>
2022-05-31 17:27:02 -0400

    add new file 05-31-2022.js with new kata challenge

---
+++ 05-May/05-31-2022.js
@@ -0,0 +1,7 @@
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+const a = "code"
+const b = "wa.rs"
+const name = a + b
+
+//*****I was able to fix the bug and get the variable corrected. 
