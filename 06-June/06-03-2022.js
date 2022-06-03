ec73b74af2e99d6bd12ca7f939a1c03d319d9d86
Harvey S <hlsamuel00@yahoo.com>
2022-06-03 15:06:50 -0400

    add new file 06-03-2022.js with new kata challenge

---
+++ 06-June/06-03-2022.js
@@ -0,0 +1,19 @@
+// Is the string uppercase?
+// Task
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function() {
+    return [...this].every(x => x == x.toUpperCase())
+}
+
+//*****I was excited that I got this on the first try!! My progress astounds me a lot of times. 
