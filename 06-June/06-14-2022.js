d1faa6190320afe216798135a9b6208ab5bda66c
Harvey S <hlsamuel00@yahoo.com>
2022-06-14 21:07:37 -0400

    add new file 06-14-2022.js with new kata challenge

---
+++ 06-June/06-14-2022.js
@@ -0,0 +1,14 @@
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name) {
+    return 'Hello, ' + name
+}
+
+//*****I was able to get this kata challenge very easily. I also refactored the code to an arrow function below:
+
+const sayHello = (name) => 'Hello, ' + name
