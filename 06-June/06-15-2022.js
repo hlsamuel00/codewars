c520753905a8c76e68c9f854167f55f5fb88d790
Harvey S <hlsamuel00@yahoo.com>
2022-06-15 23:02:38 -0400

    add new file 06-15-2022.js with new kata challenge

---
+++ 06-June/06-15-2022.js
@@ -0,0 +1,10 @@
+//Remember the spongebob meme that is meant to make fun of people by repeating what they say in a mocking way?
+
+// You need to create a function that converts the input into this format, with the output being the same string expect there is a pattern of uppercase and lowercase letters.
+
+// Example:
+
+// input:  "stop Making spongebob Memes!"
+// output: "StOp mAkInG SpOnGeBoB MeMeS!"
+
+const spongeMeme = sentence => [...sentence].map((s,i) => !(i % 2) ? s.toUpperCase() : s.toLowerCase()).join('')
