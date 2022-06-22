cf0c7a94911057bb16267e8fb3a6411958b97975
Harvey S <hlsamuel00@yahoo.com>
2022-06-22 10:02:58 -0400

    add new file 06-22-2022.js with new kata challenge

---
+++ 06-June/06-22-2022.js
@@ -0,0 +1,12 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+
+
+const oddCount = n => Math.floor(n/2)
+
+//******I was able to get this kata fairly easily, but recognizing the patter is what saved me when focusing on larger numbers. 
+
+//=============================================================================================================
