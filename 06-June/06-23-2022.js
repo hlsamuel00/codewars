0a43355ee76c6b783dfed0bda44ff8f08d9ea9b4
Harvey S <hlsamuel00@yahoo.com>
2022-06-23 12:19:19 -0400

    add new kata challenge to 06-23-2022 .js

--- 06-June/06-23-2022.js
+++ 06-June/06-23-2022.js
@@ -30,3 +30,28 @@ const moonRating = (rating, m = Math.round(rating)) => `${'o'.repeat(m / 2)}${'c
 
 
 //=============================================================================================================
+
+// Task
+// Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+// Notes
+// Multiple correct answers may be possible. In this case, return any one of them.
+// The given index will be inside the given array.
+// The given array will, therefore, never be empty.
+
+// Example
+// leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+// leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+
+
+function leastLarger(a,i) {
+    let sorted = a.map(x => x).sort((el,el2) => el - el2)
+    return a.indexOf(sorted.find(x => x > a[i]))
+}
+
+//*****I was able to get the code above, but i refactored to the code below. 
+
+const leastLarger = (a,i) => a.indexOf([...a].sort((el,el2) => el - el2).find(x => x > a[i]))
+
+
+//=============================================================================================================
