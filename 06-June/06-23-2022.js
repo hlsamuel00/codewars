0914dc8cbda9533ed86255382bac4237f40201b9
Harvey S <hlsamuel00@yahoo.com>
2022-06-23 13:17:58 -0400

    add new kata challenge to 06-23-2022.js

--- 06-June/06-23-2022.js
+++ 06-June/06-23-2022.js
@@ -55,3 +55,22 @@ const leastLarger = (a,i) => a.indexOf([...a].sort((el,el2) => el - el2).find(x 
 
 
 //=============================================================================================================
+
+
+// Task
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
+function sortArray(array) {
+    let odd = array.filter(x => !!(x % 2)).sort((el,el2) => el-el2)
+    return array.map((x,i) => !(x % 2) ? x : odd.shift())
+}
+
+//*****I was able to get this kata challenge after a bit and it was a 6kyu!!! I also took a bit of time to refactor the code below:
+
+const sortArray = (array, odd = array.filter(x => x % 2).sort((el,el2) => el-el2)) => array.map((x,i) => !(x % 2) ? x : odd.shift())
