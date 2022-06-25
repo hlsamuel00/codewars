c4e6a8c46a62b8bd4a821d08c858bccfbcf5b1da
Harvey S <hlsamuel00@yahoo.com>
2022-06-24 22:40:20 -0400

    add new file 06-24-2022.js with new kata challenge

---
+++ 06-June/06-24-2022.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+const replaceDots = (str) => str.replace(/\./g, '-')
+
+//*****I was able to get this kata very easily. I chose this to do as it's late and my brain is a bit fried from tying to tackle a 6kyu problem. 
