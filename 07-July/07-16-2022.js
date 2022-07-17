56188743a4796471040cadaf4c26030d7f986b97
Harvey S <hlsamuel00@yahoo.com>
2022-07-16 23:04:13 -0400

    add new file 07-16-2022.js with new kata challenge

---
+++ 07-July/07-16-2022.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+const position = (letter) => `Position of alphabet: ${letter.toLowerCase().charCodeAt(0) - 96}`
+
+//*****I was able to get this kata very easily. In looking through the solutions, the solutions provided were very similar to the one I wrote. 
