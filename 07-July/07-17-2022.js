5d3439774d1757bea47de1e8cfeddf3e922cbcbc
Harvey S <hlsamuel00@yahoo.com>
2022-07-17 10:20:07 -0400

    add new file 07-17-2022.js with new kata challenge

---
+++ 07-July/07-17-2022.js
@@ -0,0 +1,16 @@
+// Given a number return the closest number to it that is divisible by 10.
+
+// Example input:
+
+// 22
+// 25
+// 37
+// Expected output:
+
+// 20
+// 30
+// 40
+
+const closestMultiple10 = num => Math.round(num / 10) * 10
+
+//*****I was able to get this kata fairly easily. I remembered doing something similar and how I went about getting the solution before. I was able to draw on that information and complete this kata. 
