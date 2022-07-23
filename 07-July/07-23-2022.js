192569a421117fd757383348e717561f3abcda89
Harvey S <hlsamuel00@yahoo.com>
2022-07-23 19:14:21 -0400

    add new kata challenge to 07-23-2022.js

--- 07-July/07-23-2022.js
+++ 07-July/07-23-2022.js
@@ -25,4 +25,25 @@ function span(arr, predicate) {
     for(var i = 0; i<arr.length && predicate(arr[i]); i++)
     return [arr.slice(0,i), arr.slice(i)]
 }
-  
+
+
+
+//=============================================================================================================
+
+
+// Here's another staple for the functional programmer. You have a sequence of values and some predicate for those values. You want to get the longest prefix of elements such that the predicate is true for each element. We'll call this the takeWhile function. It accepts two arguments. The first is the sequence of values, and the second is the predicate function. The function does not change the value of the original sequence.
+
+// Example:
+// sequence : [2,4,6,8,1,2,5,4,3,2]
+// predicate: is an even number
+// result   : [2,4,6,8]
+// Your task is to implement the takeWhile function.
+
+// If you've got a span function lying around, this is a one-liner! Also, if you liked this problem, you'll surely love the dropWhile function.
+
+function takeWhile (arr, pred) {
+    const ind = arr.indexOf(arr.find(x => !pred(x)))
+    return arr.slice(0,(ind>=0 ? ind : arr.length))
+}
+
+//*****I was able to get this kata very easily thanks to the previous challenge. I will complete the final challenge of this series on another day.
