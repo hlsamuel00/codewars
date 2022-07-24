c8b6f448e3e4ff44c43d73ccdf3bd238dcf23778
Harvey S <hlsamuel00@yahoo.com>
2022-07-24 09:26:18 -0400

    add new file 07-24-2022.js with new kata challenge

---
+++ 07-July/07-24-2022.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// Yet another staple for the functional programmer. You have a sequence of values and some predicate for those values. You want to remove the longest prefix of elements such that the predicate is true for each element. We'll call this the dropWhile function. It accepts two arguments. The first is the sequence of values, and the second is the predicate function. The function does not change the value of the original sequence.
+
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
+// var seq = [2,4,6,8,1,2,5,4,3,2];
+
+// dropWhile(seq, isEven) // -> [1,2,5,4,3,2]
+// Your task is to implement the dropWhile function. If you've got a span function lying around, this is a one-liner! Alternatively, if you have a takeWhile function on your hands, then combined with the dropWhile function, you can implement the span function in one line. This is the beauty of functional programming: there are a whole host of useful functions, many of which can be implemented in terms of each other.
+
+function dropWhile(arr, pred) {
+    const ind = arr.indexOf(arr.find(x => !pred(x)))
+    return arr.slice((ind >= 0 ? ind : arr.length))
+  }
+
+//*****I was able to get this kata very easily due to having done the span, but I wanted to refactor the code a bit to utilize while since the name of the kata is dropWhile. I refactored the code below for a while loop.
+
+function dropWhile(arr, pred, ind = 0) {
+    while(ind < arr.length && pred(arr[ind])){ ind++ }
+    return arr.slice(ind)
+}
+
+
+//=============================================================================================================
