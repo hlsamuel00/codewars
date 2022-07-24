428362d97822a0e924a156e4ec52475b0313358a
Harvey S <hlsamuel00@yahoo.com>
2022-07-24 09:43:02 -0400

    add new kata challenge to 07-24-2022.js

--- 07-July/07-24-2022.js
+++ 07-July/07-24-2022.js
@@ -44,3 +44,26 @@ function takeWhile (arr, pred, ind = 0) {
 
 
 //=============================================================================================================
+
+//*****I revisited the span function from yesterday as well to rewrite it as a function that utilizes the previously made functions. 
+
+// The span function is a good one to know. It accepts a sequence and a predicate function and returns two sequences. The first sequence contains all the elements of the argument sequence up to the item that caused the first failure of the predicate. The second returned sequence contains the rest of the original sequence. The original sequence is not modified.
+
+// For example,
+
+
+// function isEven (x) {
+//   return Math.abs(x) % 2 === 0;
+// }
+
+// var arr = [2,4,6,1,8,10];
+
+// // This is true
+// span(arr, isEven) === [[2,4,6],[1,8,10]]
+// Your task is to...wait for it... write your own span function !!!
+
+// Hint/Challenge: If you have completed the takeWhile function and the dropWhile function, then you can solve this problem in one line!
+
+function span(arr, predicate) {
+    return [takeWhile(arr,predicate), dropWhile(arr,predicate)]
+}
