08e3c4941de7ab8d94bea1ee5e0ca58f69d26440
Harvey S <hlsamuel00@yahoo.com>
2022-07-29 14:14:37 -0400

    add new file 07-29-2022.js with new kata challenge

---
+++ 07-July/07-29-2022.js
@@ -0,0 +1,7 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+const sumMix = x => x.reduce((acc,el) => +acc + parseInt(el))
+
+//*****This kata was very easy to tackle.
