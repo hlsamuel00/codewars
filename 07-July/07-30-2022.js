16ce3929dd0c7794b9f79a3db559566626ca6a30
Harvey S <hlsamuel00@yahoo.com>
2022-07-30 13:29:46 -0400

    add new file 07-30-2022.js with new kata challenge

---
+++ 07-July/07-30-2022.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+const countBy = (x, n) => Array.from({length: n}, (_,i) => ++i * x)
+
+//*****I was able to get this kata challenge fairly easily. I'm most impressed by my ability to increment the index prior to using it, versus using (i + 1).
