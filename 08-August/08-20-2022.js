ea1bd33bded14f11e194ddf224edbe57cc36d82b
Harvey S <hlsamuel00@yahoo.com>
2022-08-20 16:34:46 -0400

    add new file 08-20-2022.js with new kata challenge

---
+++ 08-August/08-20-2022.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+// Simples!
+
+//P: input will be an array of values [tail, body, head]
+//R: output will be the array arranged accordingly [head, body, tail]
+//E: fixTheMeerkat(['tail', 'body', 'head']) => ['head', 'body', 'tail']
+  // fixTheMeerkat(['tails', 'body', 'heads']) => ['heads', 'body', 'tails']
+  // fixTheMeerkat(['bottom', 'middle', 'top']) => ['top', 'middle', 'bottom']
+//Question: will the array always be arranged the same?
+
+const fixTheMeerkat = (arr) =>{
+    //reverse the array (arr.reverse())
+    //return the result
+    return arr.reverse()
+}
+
+//=============================================================================================================
+
