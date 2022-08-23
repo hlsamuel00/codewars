07935512efe8cd498f6cc905d06dbcb8599ba2d8
Harvey S <hlsamuel00@yahoo.com>
2022-08-23 14:40:13 -0400

    add new kata challenge to file 08-23-2022.js

--- 08-August/08-23-2022.js
+++ 08-August/08-23-2022.js
@@ -30,3 +30,33 @@ const checkAlive = health => health > 0
 
 
 //=============================================================================================================
+
+// DESCRIPTION:
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+//P: input will be a string a name of a programmer
+//R: output should be an integer for the number of lightsabers owned. Zach is the only owner of lightsabers.
+//E: howManyLightsabersDoYouOwn('Branden') => 0
+  // howManyLightsabersDoYouOwn('Harvey') => 0
+  // howManyLightsabersDoYouOwn('Zach') => 18
+
+const howManyLightsabersDoYouOwn = name =>{
+    // make name lowercase to ensure there are no issues with alternate capitalization (name.toLowerCase())
+    // set up conditional to determine if the name matches and provide the correct response
+    // include a part of the conditional that tests for an empty argument
+    //return the results
+    return name && name.toLowerCase() == 'zach' ? 18 : 0
+}
+
+//=============================================================================================================
