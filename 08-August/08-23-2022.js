516675aa0b84488375a33fa4005a5e02c308611d
Harvey S <hlsamuel00@yahoo.com>
2022-08-23 15:01:56 -0400

    add new kata challenge to file 08-23-2022.js

--- 08-August/08-23-2022.js
+++ 08-August/08-23-2022.js
@@ -60,3 +60,45 @@ const howManyLightsabersDoYouOwn = name =>{
 }
 
 //=============================================================================================================
+
+// DESCRIPTION:
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+// For example, a multiplication table (string) for number == 5 looks like below:
+
+// 1 * 5 = 5
+// 2 * 5 = 10
+// 3 * 5 = 15
+// 4 * 5 = 20
+// 5 * 5 = 25
+// 6 * 5 = 30
+// 7 * 5 = 35
+// 8 * 5 = 40
+// 9 * 5 = 45
+// 10 * 5 = 50
+// P. S. You can use \n in string to jump to the next line.
+
+// Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
+
+//P: input will be an integer between 1 and 10
+//R: output should be a list of the multiplication table from 1 to 10 with the number provided
+//E: multiTable(5) => // 1 * 5 = 5
+                      // 2 * 5 = 10
+                      // 3 * 5 = 15
+                      // 4 * 5 = 20
+                      // 5 * 5 = 25
+                      // 6 * 5 = 30
+                      // 7 * 5 = 35
+                      // 8 * 5 = 40
+                      // 9 * 5 = 45
+                      // 10 * 5 = 50
+
+const multiTable = number => {
+    //set up an array length of 10 Array.from({length: 10})
+        // use the map to set the element to equal the full multiplication table (_,i) =>
+    // join them using the new line syntax (.join(\n))
+    // return the result
+    return Array.from({length: 10}, (_,i) => `${++i} * ${number} = ${i * number}`).join('\n')
+}
+
+//=============================================================================================================
