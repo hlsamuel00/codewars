10825a79f2d8eb43a84eb274777ddddc75f7babe
Harvey S <hlsamuel00@yahoo.com>
2022-08-29 13:19:25 -0400

    add new kata challenge to file 08-29-2022.js

--- 08-August/08-29-2022.js
+++ 08-August/08-29-2022.js
@@ -70,3 +70,36 @@ const add = (val1, val2) => {
 }
 
 //=============================================================================================================
+
+// DESCRIPTION:
+
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+// Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+// If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+// Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+// var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+// var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+// aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+// aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+// Happy hacking!
+
+
+//P: input will be two strings
+//R: output should be a new string with the names replaced with the objects provided or 'Your name must start with a letter from A - Z.' if the name doesnt start with a letter
+//E: aliasGen('Larry', 'Brentwood') => 'Logic Bomb'
+  // aliasGen('123abc', 'Petrovic') => 'Your name must start with a letter from A - Z.'
+
+const aliasGen = (fName, lName) => {
+    // get the first letter of each of the arguments (fName[0] lName[0])
+    // establish conditional to determine if the name starts with a letter (!+fName[0] && !+lName)
+        // create condtional outcomes (? `${firstName[fName[0].toUpperCase()]} ${surname[lName[0].toUpperCase()]}` : 'Your name must start with a letter from A - Z.')
+    // return the result
+    return !+fName[0] && !+lName ? `${firstName[fName[0].toUpperCase()]} ${surname[lName[0].toUpperCase()]}` : 'Your name must start with a letter from A - Z.'
+}
