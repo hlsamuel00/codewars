571864b72f192d94ec8b9310f8dd0aec869c23dc
Harvey S <hlsamuel00@yahoo.com>
2022-08-31 10:23:42 -0400

    add new file 08-31-2022.js with new kata challenge

---
+++ 08-August/08-31-2022.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+//P: input will be a number
+//R: output should be the number converted to a string
+//E: numberToString(123) => '123'
+  // numberToString(999) => '999'
+  // numberToString(-100) => '-100'
+
+const numberToString = num => {
+    // convert number to string (num.toString())
+    // return the result
+    return num.toString()
+}
+
+//=============================================================================================================
+
