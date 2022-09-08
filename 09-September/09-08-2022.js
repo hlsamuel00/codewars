1bc0721aca650d3138f5474ed4d7fe35595da049
Harvey S <hlsamuel00@yahoo.com>
2022-09-08 19:27:29 -0400

    add new file 09-08-2022.js with new kata challenge

---
+++ 09-September/09-08-2022.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+// Code provided:
+function swapValues() {
+    var args = Array.prototype.slice.call(arguments);
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+// Updated code:
+function swapValues() {
+    var args = Array.prototype.slice.call(arguments)[0];
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+}
+
+// My code:
+function swapValues() {
+    Array.prototype.slice.call(arguments)[0].reverse();
+}
