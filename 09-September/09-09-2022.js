477beaccd98d41a10dc1291c2abc1237b8ef77f8
Harvey S <hlsamuel00@yahoo.com>
2022-09-09 15:13:51 -0400

    add new kata challenge to file 09-09-2022.js

--- 09-September/09-09-2022.js
+++ 09-September/09-09-2022.js
@@ -33,3 +33,31 @@ const printArray = (arr) => {
     // return the result
     return arr.join()
 }
+
+//=============================================================================================================
+
+// DESCRIPTION:
+// SpeedCode #2 - Array Madness
+// Objective
+// Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+// E.g.
+
+// arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+// Get your timer out. Are you ready? Ready, get set, GO!!!
+
+//P: input will be two arrays with length > 1
+//R: output should be a boolean regarding whether the sum of the squares of the first array is greater than the sum of the cubes of the second array
+//E: arrayMadness([4, 5, 6], [1, 2, 3]) => true
+//Question: Will both arrays be equal in length?
+
+const arrayMadness = (arr1, arr2) => {
+    // square and sum the first array (arr1.reduce((acc,el) => (acc + el**2), 0))
+    // cube and sum the second array (arr2.reduce((acc,el) => (acc + el**3), 0))
+    // create comparison of the sums
+    // return the result
+    return arr1.reduce((acc,el) => acc + el**2, 0) > arr2.reduce((acc,el) => acc + el**3 , 0)
+}
+
+//=============================================================================================================
+
