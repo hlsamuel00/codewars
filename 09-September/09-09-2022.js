c9ce2c203f8f66f17ec4139d3fca6ed6a8fd0aa2
Harvey S <hlsamuel00@yahoo.com>
2022-09-09 15:32:37 -0400

    add new kata challenge to file 09-09-2022.js

--- 09-September/09-09-2022.js
+++ 09-September/09-09-2022.js
@@ -61,3 +61,34 @@ const arrayMadness = (arr1, arr2) => {
 
 //=============================================================================================================
 
+// DESCRIPTION:
+// Find Mean
+// Find the mean (average) of a list of numbers in an array.
+
+// Information
+// To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+// For an example list of 1, 3, 5, 7
+
+// 1. Add all of the numbers
+
+// 1+3+5+7 = 16
+// 2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+// 16/4 = 4
+// 3. The mean (or average) of this list is 4
+
+//P: input will be an array
+//R: output should be the average (mean) of the array
+//E: findAverage([1]) => 1
+  // findAverage([1, 3, 5, 7]) => 4
+
+const findAverage = (arr) => {
+    // reduce average to it's sums (arr.reduce((acc,el) => (acc += el) && acc, 0))
+    // divide sum by the length of the array (/arr.length)
+    // return the result
+    return arr.reduce((acc, el) => (acc += el) & acc, 0)/arr.length
+}
+
+//=============================================================================================================
+
