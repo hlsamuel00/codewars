faaeaabb71c83512ce6ee6a179bbe7bc07e843e2
Harvey S <hlsamuel00@yahoo.com>
2022-09-09 14:38:34 -0400

    add new kata challenge to file 09-09-2022.js

--- 09-September/09-09-2022.js
+++ 09-September/09-09-2022.js
@@ -13,3 +13,23 @@ websites.splice(0,0,'codewars')
 websites.unshift('codewars')
 
 //=============================================================================================================
+
+// DESCRIPTION:
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+//P: input will be an array
+//R: output should be the array converted to a string separated by commas
+//E: printArray([2, 4, 5, 2]) => '2,4,5,2'
+  // printArray(['h', 'e', 'l', 'l', 'o']) => 'h,e,l,l,o'
+
+const printArray = (arr) => {
+    // join array with the comma delimiter (arr.join(','))
+    // return the result
+    return arr.join()
+}
