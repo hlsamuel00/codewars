fb4d6ebeaef492bfb040ec1ccaf710c80e707d60
Harvey S <hlsamuel00@yahoo.com>
2022-09-09 17:54:01 -0400

    add new kata challenge to file 09-09-2022.js

--- 09-September/09-09-2022.js
+++ 09-September/09-09-2022.js
@@ -92,3 +92,26 @@ const findAverage = (arr) => {
 
 //=============================================================================================================
 
+// DESCRIPTION:
+// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+
+//P: input will be an array of numbers
+//R: output should be a new array of the [youngest age, oldest age, and the distance between both]
+//E: differenceInAges([82, 15, 6, 38, 35]) => [6, 82, 76]
+  // differenceInAges([57, 99, 14, 32]) => [14, 99, 85]
+
+
+
+const differenceInAges = (ages) => {
+    //obtain the maximum of the array
+    const max = (arr) => Math.max(...arr)
+    //obtain the minimum of the array
+    const min = (arr) => Math.min(...arr)
+    //obtain the difference of the two
+    const difference = (arr) => max(arr) - min(arr)
+    // create tuple of the values
+    // return the result
+    return [min(arr), max(arr), difference(arr)]
+}
