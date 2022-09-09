58d7bb25dd5b048360acf896a1466cd549cfc468
Harvey S <hlsamuel00@yahoo.com>
2022-09-09 14:38:07 -0400

    add new file 09-09-2022.js with a new kata challenge

---
+++ 09-September/09-09-2022.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+websites.push('codewars')
+// OR
+websites.splice(0,0,'codewars')
+// OR
+websites.unshift('codewars')
+
+//=============================================================================================================
