31016765e398afe05872741638516f72fc3ee4e4
Harvey S <hlsamuel00@yahoo.com>
2022-09-12 11:34:53 -0400

    add new kata challenge to file 09-12-2022.js

--- 09-September/09-12-2022.js
+++ 09-September/09-12-2022.js
@@ -24,3 +24,30 @@ Array.prototype.filter = function(func){
 
 //=============================================================================================================
 
+// DESCRIPTION:
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+//P: input will be an array and optionally an integer
+//R: return should be the the first element if the integer is not defined, if the integer is defined, the element should be length of the number provided.
+//E: first(arr) //=> ['a'];
+  // first(arr, 2) //=> ['a', 'b']
+  // first(arr, 3) //=> ['a', 'b', 'c'];
+  // first(arr, 0) //=> [];
+
+const first = (arr, idx = 1) => {
+    // slice the first index of the array and set idx = 1 or the value entered (.slice(0, idx))
+    // return the result
+    return arr.slice(0, idx)
+}
+
+//=============================================================================================================
+
