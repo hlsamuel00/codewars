b23f74c1af69bcbda6110e7537f031880c6c395f
Harvey S <hlsamuel00@yahoo.com>
2022-09-12 16:50:19 -0400

    add new kata challenge to file 09-12-2022.js

--- 09-September/09-12-2022.js
+++ 09-September/09-12-2022.js
@@ -84,3 +84,39 @@ const arr2bin = (arr) => arr.reduce((acc,el) => typeof(el) === 'number' ? acc + 
 
 //=============================================================================================================
 
+// DESCRIPTION:
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4
+
+
+//P: input will be an array of 1's and 0's
+//R: output should be the array of binary converted to a number 
+//E: binaryArrayToNumber([0, 0, 0, 1]) => 1
+  // binaryArrayToNumber([0, 0, 1, 0]) => 2
+  // binaryArrayToNumber([0, 1, 0, 1]) => 5
+  // binaryArrayToNumber([1, 0, 0, 1]) => 9
+  // binaryArrayToNumber([0, 0, 1, 0]) => 2
+  // binaryArrayToNumber([0, 1, 1, 0]) => 6
+  // binaryArrayToNumber([1, 1, 1, 1]) => 15
+  // binaryArrayToNumber([1, 0, 1, 1]) => 11
+//Question: Will the array always be 1's and 0's
+
+const binaryArrayToNumber = (arr) => {
+    // combine the array into a string (arr.join(''))
+    // convert string binary to an integer (parseInt(arr.join(''), 2))
+    // return the result
+    return parseInt(arr.join(''), 2)
+}
