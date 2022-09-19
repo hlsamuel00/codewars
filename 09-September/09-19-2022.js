e04fc07834c6354f293d0bf7cef4f80760235dc2
Harvey S <hlsamuel00@yahoo.com>
2022-09-19 11:45:30 -0400

    add new file 09-19-2022.js with new kata challenge

---
+++ 09-September/09-19-2022.js
@@ -0,0 +1,29 @@
+// DESCRIPTION:
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+//P: input will be a string
+//R: output will be an array of integers that correspond with the index of each capital letter
+//E: capitals('CodEWaRs') => [0,3,4,6]
+
+const captials = (word) => {
+    // split word into an array ([...word])
+    // filter the array to only include capital letters (.filter(el => el == el.toUpperCase()))
+    // map over the array to change the letter to it's corresponding index (.map(el => word.indexOf(el)))
+    // return the result
+    return [...word].filter(el => el == el.toUpperCase()).map(el => word.indexOf(el))
+}
+
+// Refactored code //
+
+const capitals = (word) => [...word].reduce((acc,el) => (/[A-Z]/.test(el) ? [...acc, el] : acc) && acc, [])
+    // split word into an array ([...word])
+    // filter the array to only include capital letters (.filter(el => el == el.toUpperCase()) or .filter(el => /[A-Z]/.test(el)))
+    // map over the array to change the letter to it's corresponding index (.map(el => word.indexOf(el)))
+        // utilize the reduce method to streamline the map and filter:
+            //.reduce((acc,el) => (el == el.toUpperCase() ? acc.push(word.indexOf(el))) && acc, [])
+            //.reduce((acc,el) => /[A-Z]/.test(el) ? [...acc, word.indexOf(el)] : acc, [])
+    // return the result
