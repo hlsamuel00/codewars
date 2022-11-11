07e4b3511dead2ed3ee9d69d60e90555f475ab5a
Harvey S <hlsamuel00@yahoo.com>
2022-11-11 11:05:41 -0500

    add new file 11-11-2022.js with new kata challenge

---
+++ 11-November/11-11-2022.js
@@ -0,0 +1,14 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+function findOutlier(integers){
+    const odd = integers.filter(nums => nums % 2)
+    const even = integers.filter(nums => !(nums % 2))
+    return odd.length > 1 ? even[0] : odd[0]
+}
